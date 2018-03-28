9f5f971c92f633d8c6a4c63d696674a9ff7e8acd
moritzmhmk <moritzmhmk@gmail.com>
2018-03-28 15:47:58 +0200

    update download url

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
+
+version = '0.0.1'
+url = 'https://github.com/moritzmhmk/wsgi-graphql'
+download_url = '{}/archive/v{}.tar.gz'.format(url, version)
+
 setup(
     name='wsgi_graphql',
     py_modules=['wsgi_graphql'],
-    version='0.0.1',
+    version=version,
     description='Create a WSGI Application for a GraphQL schema.',
     long_description=open('README.rst').read(),
     author='Moritz K.',
     author_email='moritzmhmk@googlemail.com',
-    url='https://github.com/moritzmhmk/wsgi-graphql',
-    download_url='https://github.com/moritzmhmk/wsgi-graphql/archive/0.0.1.tar.gz',
+    url=url,
+    download_url=download_url,
     install_requires=['graphql-server-core'],
     license='MIT',
     keywords=['graphql', 'wsgi', 'api', 'rest'],
