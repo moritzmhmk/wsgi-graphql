64671d9efd325d95698d18c343067e9ea417c7ed
moritzmhmk <moritzmhmk@gmail.com>
2018-03-28 15:51:12 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '0.0.1'
+version = '0.0.2'
 url = 'https://github.com/moritzmhmk/wsgi-graphql'
 download_url = '{}/archive/v{}.tar.gz'.format(url, version)
 
